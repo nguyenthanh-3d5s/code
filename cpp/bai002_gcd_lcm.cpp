e2e51c83937699bd7172542cf7ddac8d773ece78
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-11-07 18:57:19 +0700

    Create bai002_gcd_lcm.cpp

---
+++ cpp/bai002_gcd_lcm.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+using ll = long long;
+
+int gcd(int &, int &);
+int lcm(int &, int &);
+
+int main() {
+    int a, b; cin >> a >> b;
+
+    cout << gcd(a, b);
+
+    return 0;
+}
+
+int gcd(int &a, int &b) {
+    while(b) {
+        int tmp = a % b;
+        a = b;
+        b = tmp;
+    }
+    return a;
+}
+
+int lcm(int &a, int &b) {
+    return a / gcd(a, b) * b;
+}
