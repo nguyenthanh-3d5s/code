5e7737e056cb70094ded392486f63cc5d015b0ce
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-11-07 18:32:50 +0700

    Create bai001_fasio.cpp

---
+++ cpp/bai001_fasio.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
