e250f11bc83e38db3ee7add0ce273a94f3430211
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-11-07 19:38:21 +0700

    Update bai001_fasio.cpp

--- cpp/bai001_fasio.cpp
+++ cpp/bai001_fasio.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 using ll = long long;
 
+void fasio();
+
 int main() {
+    fasio();
+    return 0;
+}
+
+void fasio() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    return 0;
 }
