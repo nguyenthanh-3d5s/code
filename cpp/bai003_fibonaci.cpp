4c268a7984cc6312520acebe6aaf27650d7151f5
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-11-07 19:33:50 +0700

    Update bai003_fibonaci.cpp

--- cpp/bai003_fibonaci.cpp
+++ cpp/bai003_fibonaci.cpp
@@ -3,23 +3,30 @@
 using namespace std;
 
 using ll = long long;
+#define sz(a) (int) (a).size()
 
 typedef vector<int> vi;
+
+void fibo(vi &);
 
 int main() {
     int n = 10;
     vi arr(n);
 
+    fibo(arr)
+    
+    return 0;
+}
+
+void fibo(vi &arr) {
     arr[0] = 0;
     arr[1] = 1;
 
-    for(int i = 2; i < n; i++) {
+    for(int i = 2; i < sz(arr); i++) {
         arr[i] = arr[i - 1] + arr[i - 2];
     }
 
     for(int i : arr) {
         cout << i << " ";
     }
-
-    return 0;
 }
