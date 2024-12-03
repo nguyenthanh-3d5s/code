c76c494f0c9f6de6cc35c376e4f010d981321a48
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-12-03 15:16:26 +0700

    Create bai006_so_fibonacci.cpp

---
+++ cpp/bai006_so_fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+long long fibonacci(int n) {
+    if (n <= 1) return n;
+    long long a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n) << endl;
+    return 0;
+}
