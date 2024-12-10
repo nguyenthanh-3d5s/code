db6794bbfe3105f4f2ae5fa1f4a586184fd0d2a8
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-12-10 14:39:59 +0700

    Update bai002_phan_tich_thua_so_nguyen_to.cpp

--- cpp/bai002_phan_tich_thua_so_nguyen_to.cpp
+++ cpp/bai002_phan_tich_thua_so_nguyen_to.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void primeFactorization(int n) {
-    for (int i = 2; i <= n; i++) {
-        while (n % i == 0) {
+void Prime_fact(int);
+
+int main() {
+    int a; cin >> a;
+    Prime_fact(a);
+    return 0;
+}
+
+void Prime_fact(int n) {
+    if(n < 2) return;
+
+    int _n = sqrt(n) + 1;
+
+    for(int i = 2; i < _n; i++) {
+        while(n % i == 0) {
             cout << i << " ";
             n /= i;
         }
+        _n = sqrt(n) + 1;
     }
-    cout << endl;
-}
 
-int main() {
-    int n;
-    cin >> n;
-    primeFactorization(n);
-    return 0;
+    if(n > 1) cout << n;
 }
