2bf2abbcecfb593b784e9457dfa6928d3db311e0
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-12-13 14:14:24 +0700

    Update and rename bai001_gcd.cpp to bai001_gcd_lcm.cpp

--- code/bai001_gcd_lcm.cpp
+++ code/bai001_gcd_lcm.cpp
@@ -4,12 +4,14 @@ using namespace std;
 using ll = long long;
 
 int gcd(int, int);
+int lcm(int, int);
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout << gcd(10, 15);
+    cout << gcd(10, 15) << "\n";
+    cout << lcm(10, 15) << "\n";
 
     return 0;
 }
@@ -21,4 +23,8 @@ int gcd(int a, int b) {
         b = tmp;
     }
     return a;
+}
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
 }
