298f117d64bcb0c72f18fca0cdf374efdc534369
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2024-12-31 13:17:46 +0700

    Update bai006_if.py

--- python/bai006_if.py
+++ python/bai006_if.py
@@ -4,6 +4,11 @@ def main():
     if a > 0:
         print("a lon hon 0")
 
+    if a < 0:
+        print("a nho hon 0")
+    elif a == 0:
+        print("a bang 0")
+
     if a % 2 == 0:
         print("a chan")
     else:
