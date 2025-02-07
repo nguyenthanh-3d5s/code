47edd0b83549a2992d31ef0f21b5aee8ae2f9467
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-07 14:28:59 +0700

    Update 07_sang_so_nguyen_to.cpp

--- cpp/07_sang_so_nguyen_to.cpp
+++ cpp/07_sang_so_nguyen_to.cpp
@@ -1,1 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+using ll = long long;
+const int n = 1e7 + 1;
+bool arr[n];
+
+void fibo();
+
+int main() {
+    #ifndef TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    fibo();
+    cout << arr[n - 1] << endl;
+
+    return 0;
+}
+
+void fibo() {
+    for(int i = 0; i < n; i++) {
+        arr[i] = true;
+    }
+
+    arr[0] = arr[1] = false;
+
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(arr[i]) {
+            for(int j = i * i; j < n; j += i) {
+                arr[j] = false;
+            }
+        }
+    }
+}
