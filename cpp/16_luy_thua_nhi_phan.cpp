dc292462f96f44dbf52a620c16d21d32f9cd6988
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-10 15:33:08 +0700

    Create 16_luy_thua_nhi_phan.cpp

---
+++ cpp/16_luy_thua_nhi_phan.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const int mod = 1e9 + 7;
+
+ll luy_thua(ll, ll);
+
+int main() {
+    #ifndef TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll a, b;
+    cin >> a >> b;
+
+    cout << luy_thua(a, b) << endl;
+
+    return 0;
+}
+
+ll luy_thua(ll a, ll b) {
+    ll res = 1;
+
+    while(b) {
+        if(b % 2 != 0) {
+            res = (res * a) % mod;
+        }
+        a = (a * a) % mod;
+        b /= 2;
+    }
+
+    return res;
+}
