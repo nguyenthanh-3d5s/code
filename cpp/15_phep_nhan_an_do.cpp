6a23f61685e7274c4e34584063b69232aa15a249
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-10 14:43:31 +0700

    Update 15_phep_nhan_an_do.cpp

--- cpp/15_phep_nhan_an_do.cpp
+++ cpp/15_phep_nhan_an_do.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+using ll = long long;
+const int mod = 1e9 + 7;
+
+ll phep_nhan(ll, ll);
+
+int main() {
+    #ifndef TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll a, b;
+    cin >> a >> b;
+
+    cout << phep_nhan(a, b) << endl;
+
+    return 0;
+}
+
+ll phep_nhan(ll a, ll b) {
+    ll sum = 0;
+
+    while(b) {
+        if(b % 2 != 0) {
+            sum = (sum + a) % mod;
+        }
+        a = (a * 2) % mod;
+        b /= 2;
+    }
+
+    return sum;
+}
