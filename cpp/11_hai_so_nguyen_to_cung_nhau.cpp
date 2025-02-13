c36c1628984e64e4a5c0653ee783bef0da72439e
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-13 13:22:07 +0700

    Update 11_hai_so_nguyen_to_cung_nhau.cpp

--- cpp/11_hai_so_nguyen_to_cung_nhau.cpp
+++ cpp/11_hai_so_nguyen_to_cung_nhau.cpp
@@ -1,1 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+using ll = long long;
+
+int gcd(int, int);
+
+int main() {
+    #ifndef TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+
+    for(int i = a; i <= b; i++) {
+        for(int j = i + 1; j <= b; j++) {
+            if(gcd(i, j) == 1) {
+                cout << "(" << i << "," << j << ")" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int gcd(int a, int b) {
+    while(b) {
+        int tmp = a % b;
+        a = b;
+        b = tmp;
+    }
+
+    return a;
+}
