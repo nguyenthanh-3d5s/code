1f939efa92bfd471ea9e91f81e245a700de05c13
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-13 21:36:33 +0700

    Create vector.cpp

---
+++ cpp0/vector.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ii = struct {int a; int b;};
+
+using vi = vector<int>;
+using vii = vector<ii>;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int) (x).size()
+
+typedef bool (*func_cmp)(int &, int &);
+inline bool cmp(int &a, int &b) {return a > b;}
+inline bool cmp_ii(ii &i, ii &j) {return i.a > j.a;}
+
+int main() {
+    #ifndef TEST
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
