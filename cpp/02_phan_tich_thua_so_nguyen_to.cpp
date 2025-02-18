7afb7811795923a9bf517a0ece07d1aa5d38cc8c
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-18 15:47:21 +0700

    Update 02_phan_tich_thua_so_nguyen_to.cpp

--- cpp/02_phan_tich_thua_so_nguyen_to.cpp
+++ cpp/02_phan_tich_thua_so_nguyen_to.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+using ll = long long;
+
+void thua_so(int);
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a; cin >> a;
+    
+    thua_so(a);
+
+    return 0;
+}
+
+void thua_so(int n) {
+    if(n < 2) {
+        cout << n << endl;
+    }
+
+    for(int i = 2; i <= sqrt(n); i++) {
+        while(n % i == 0) {
+            cout << i << " ";
+            n /= i;
+        }
+    }
+
+    if(n > 1) {
+        cout << n << endl;
+    }
+}
