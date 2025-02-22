edbdb8df738f954ad8395ff2672a92edf9626dad
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-22 23:58:44 +0700

    Update 04_so_chinh_phuong.cpp

--- cpp/04_so_chinh_phuong.cpp
+++ cpp/04_so_chinh_phuong.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+using ll = long long;
+
+bool chinh_phuong(int);
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+
+    cout << chinh_phuong(n) << endl;
+
+    return 0;
+}
+
+bool chinh_phuong(int n) {
+    int m = sqrt(n);
+    return m * m == n;
+}
