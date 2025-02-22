0e8f8ed024d1d0a1c001ce9dbaa90cebb6a6ba9d
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-23 00:12:10 +0700

    Update 03_dem_uoc_tinh_tong_uoc.cpp

--- cpp/03_dem_uoc_tinh_tong_uoc.cpp
+++ cpp/03_dem_uoc_tinh_tong_uoc.cpp
@@ -13,25 +13,26 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int a; cin >> a;
+    int n; cin >> n;
     
-    cout << dem_uoc(a) << endl;
-    cout << tong_uoc(a) << endl;
+    cout << dem_uoc(n) << endl;
+    cout << tong_uoc(n) << endl;
 
     return 0;
 }
 
 int dem_uoc(int n) {
     int cnt = 0;
+    int m = sqrt(n);
 
-    for(int i = 1; i <= sqrt(n); i++) {
+    for(int i = 1; i <= m; i++) {
         if(n % i == 0) {
-            cnt++;
-
-            if(i != n / i) {
-                cnt++;
-            }
+            cnt += 2;
         }
+    }
+
+    if(m * m == n) {
+        cnt--;
     }
 
     return cnt;
@@ -39,15 +40,16 @@ int dem_uoc(int n) {
 
 int tong_uoc(int n) {
     int sum = 0;
+    int m = sqrt(n);
 
-    for(int i = 1; i <= sqrt(n); i++) {
+    for(int i = 1; i <= m; i++) {
         if(n % i == 0) {
-            sum += i;
-
-            if(i != n / i) {
-                sum += n / i;
-            }
+            sum += i + n / i;
         }
+    }
+
+    if(m * m == n) {
+        sum -= m;
     }
 
     return sum;
