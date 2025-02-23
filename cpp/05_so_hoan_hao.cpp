1569b97290a4cdd8cf857cd4bae56b7449d5c79d
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-02-23 14:07:37 +0700

    Update 05_so_hoan_hao.cpp

--- cpp/05_so_hoan_hao.cpp
+++ cpp/05_so_hoan_hao.cpp
@@ -1,1 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+using ll = long long;
+
+bool nguyen_to(ll);
+void hoan_hao();
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    hoan_hao();
+
+    return 0;
+}
+
+bool nguyen_to(ll n) {
+    if(n < 2) {
+        return false;
+    }
+
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void hoan_hao() {
+    for(int i = 1; i < 32; i++) {
+        if(nguyen_to(i)) {
+            ll m1 = pow(2, i) - 1;
+
+            if(nguyen_to(m1)) {
+                ll m2 = pow(2, i - 1);
+                cout << m1 * m2 << endl;
+            }
+        }
+    }
+}
