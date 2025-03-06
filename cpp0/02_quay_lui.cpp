bbcbacbc941ede209b94c48826edee156faecc54
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-06 21:39:57 +0700

    Create 02_quay_lui.cpp

---
+++ cpp0/02_quay_lui.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+using vi = vector<int>;
+#define pb push_back
+#define pp pop_back
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    vi arr;
+
+    for(int i = n; i > 0; i--) {
+        arr.pb(i);
+    }
+
+    while(!arr.empty()) {
+        cout << arr.back() << " ";
+        arr.pp();
+    }
+
+    return 0;
+}
