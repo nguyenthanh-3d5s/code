304f7854c701f0bee47f99e95ba9651a274900ef
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-11 16:21:31 +0700

    Update 12_so_thuan_nghich.cpp

--- cpp/12_so_thuan_nghich.cpp
+++ cpp/12_so_thuan_nghich.cpp
@@ -9,9 +9,6 @@ bool thuan_nghich(int);
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
 
     cout << thuan_nghich(101);
 
