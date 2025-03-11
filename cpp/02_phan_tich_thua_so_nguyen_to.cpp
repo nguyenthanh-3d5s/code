ff38fe32b264d6960fc463832fcd6d3d0441ed5a
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-11 16:19:59 +0700

    Update 02_phan_tich_thua_so_nguyen_to.cpp

--- cpp/02_phan_tich_thua_so_nguyen_to.cpp
+++ cpp/02_phan_tich_thua_so_nguyen_to.cpp
@@ -9,9 +9,6 @@ void thua_so(int);
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
 
     int n; cin >> n;
     
