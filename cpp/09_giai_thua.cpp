031eac783e1da4a9bca0b32ff1f2d079f9390167
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-11 16:21:03 +0700

    Update 09_giai_thua.cpp

--- cpp/09_giai_thua.cpp
+++ cpp/09_giai_thua.cpp
@@ -15,9 +15,6 @@ void big_int(vi &, int);
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
 
     vi arr; arr.pb(1);
 
