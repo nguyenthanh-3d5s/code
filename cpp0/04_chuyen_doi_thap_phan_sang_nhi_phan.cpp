5d437df6f10f00b9131ab35619ddb05dbb1b3c91
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-11 16:22:49 +0700

    Update 04_chuyen_doi_thap_phan_sang_nhi_phan.cpp

--- cpp0/04_chuyen_doi_thap_phan_sang_nhi_phan.cpp
+++ cpp0/04_chuyen_doi_thap_phan_sang_nhi_phan.cpp
@@ -9,9 +9,6 @@ void quay_lui(int);
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
 
     int n; cin >> n;
 
