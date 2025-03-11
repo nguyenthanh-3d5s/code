45ffcbb1e3c8e86e71ca59627b39489da149358f
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-11 16:20:09 +0700

    Update 03_dem_uoc_tinh_tong_uoc.cpp

--- cpp/03_dem_uoc_tinh_tong_uoc.cpp
+++ cpp/03_dem_uoc_tinh_tong_uoc.cpp
@@ -10,10 +10,7 @@ int tong_uoc(int);
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    
+
     int n; cin >> n;
     
     cout << dem_uoc(n) << endl;
