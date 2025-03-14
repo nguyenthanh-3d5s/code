697a97583a5004cdb0cc6324b6b133ee54257a62
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-14 12:00:25 +0700

    Update 08_tron.cpp

--- cpp0/08_tron.cpp
+++ cpp0/08_tron.cpp
@@ -6,43 +6,64 @@ using ll = long long;
 
 using vi = vector<int>;
 #define pb push_back
-#define all(x) (x).begin(), (x).end()
+#define sz(x) (int) (x).size()
 
 typedef bool (*func_cmp)(int &, int &);
 
 inline bool cmp_tang(int &a, int &b) {return a < b;}
 inline bool cmp_giam(int &a, int &b) {return a > b;}
 
+void tron(int &, int, int, int);
+void sap_xep_tron(int &, int, int);
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m;
-    cin >> n >> m;
+    int n;
+    vi arr;
 
-    int a[n], b[m];
-
-    for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i = 0; i < m; i++) cin >> b[i];
-
-    int i = 0, j = 0;
-
-    while(i < n && j < m) {
-        if(a[i] < b[j]) {
-            cout << a[i++] << " ";
-        }
-        else{
-            cout << b[j++] << " ";
-        }
+    while(cin >> n) {
+        arr.pb(n);
     }
 
-    while(i < n) {
-        cout << a[i++] << " ";
-    }
+    sap_xep_tron(arr, 0, sz(arr) - 1);
 
-    while(j < m) {
-        cout << b[j++] << " ";
+    for(int i : arr) {
+        cout << i << " ";
     }
 
     return 0;
+}
+
+void tron(int &arr, int l, int m, int r) {
+    vi a(arr.begin() + l, arr.begin() + m + 1);
+    vi b(arr.begin() + m + 1, arr.begin() + r + 1);
+
+    int i = 0, j = 0;
+
+    while(i < sz(a) && j < sz(b)) {
+        if(a[i] < b[j]) {
+            arr[l++] = a[i++];
+        }
+        else {
+            arr[l++] = b[j++];
+        }
+    }
+
+    while(i < sz(a)) {
+        arr[l++] = a[i++];
+    }
+
+    while(j < sz(b)) {
+        arr[l++] = b[j++];
+    }
+}
+
+void sap_xep_tron(int &arr, int l, int r) {
+    if(l >= r) return;
+    int m = l + (r - l) / 2;
+    sap_xep_tron(arr, l, m);
+    sap_xep_tron(arr, m + 1, r);
+    tron(arr, l, m, r);
 }
