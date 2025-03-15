f1d655e2864c88da3cf7f458cb157fd63fc8da48
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-15 16:31:16 +0700

    Update 09_tim_kiem_nhi_phan.cpp

--- cpp0/09_tim_kiem_nhi_phan.cpp
+++ cpp0/09_tim_kiem_nhi_phan.cpp
@@ -12,9 +12,7 @@ using vi = vector<int>;
 inline bool cmp_tang(int &a, int &b) {return a < b;}
 inline bool cmp_giam(int &a, int &b) {return a > b;}
 
-int tim_kiem_nhi_phan(vi &, int, int, int);
-int tim_kiem_ben_trai(vi &, int, int, int);
-int tim_kiem_ben_phai(vi &, int, int, int);
+int tim_kiem_nhi_phan(vi &, int, int, int, int = 0);
 
 int main() {
     ios::sync_with_stdio(false);
@@ -29,7 +27,7 @@ int main() {
 
     sort(all(arr), cmp_tang);
 
-    int x = tim_kiem_ben_phai(arr, 0, sz(arr) - 1, 2);
+    int x = tim_kiem_nhi_phan(arr, 0, sz(arr) - 1, 2, 2);
 
     if(x == -1) {
         cout << "khong tim thay";
@@ -41,7 +39,7 @@ int main() {
     return 0;
 }
 
-int tim_kiem_nhi_phan(vi &arr, int l, int r, int x) {
+int tim_kiem_nhi_phan(vi &arr, int l, int r, int x, int mode) {
     int res = -1;
 
     while(l <= r) {
@@ -49,47 +47,10 @@ int tim_kiem_nhi_phan(vi &arr, int l, int r, int x) {
 
         if(arr[m] == x) {
             res = m;
-            break;
-        }
-        else if(arr[m] < x) {
-            l = m + 1;
-        }
-        else {
-            r = m - 1;
-        }
-    }
-    return res;
-}
 
-int tim_kiem_ben_trai(vi &arr, int l, int r, int x) {
-    int res = -1;
-
-    while(l <= r) {
-        int m = l + (r - l) / 2;
-
-        if(arr[m] == x) {
-            res = m;
-            r = m - 1;
-        }
-        else if(arr[m] < x) {
-            l = m + 1;
-        }
-        else {
-            r = m - 1;
-        }
-    }
-    return res;
-}
-
-int tim_kiem_ben_phai(vi &arr, int l, int r, int x) {
-    int res = -1;
-
-    while(l <= r) {
-        int m = l + (r - l) / 2;
-
-        if(arr[m] == x) {
-            res = m;
-            l = m + 1;
+            if(mode == 1) r = m - 1;
+            if(mode == 2) l = m + 1;
+            if(mode == 0) break;
         }
         else if(arr[m] < x) {
             l = m + 1;
