ac82ff34548c2a39618b20c14bc2b28b8e610cbd
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-03-15 17:28:54 +0700

    Create 09_tim_kiem_nhi_phan.cpp

---
+++ cpp0/09_tim_kiem_nhi_phan.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define endl '\n'
+
+using vi = vector<int>;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int) (x).size()
+
+inline bool cmp_tang(int &a, int &b) {return a < b;}
+inline bool cmp_giam(int &a, int &b) {return a > b;}
+
+int tim_kiem_nhi_phan(vi &, int, int = 0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    vi arr;
+
+    while(cin >> n) {
+        arr.pb(n);
+    }
+
+    sort(all(arr), cmp_tang);
+
+    int x = tim_kiem_nhi_phan(arr, 2);
+
+    if(x == -1) {
+        cout << "khong tim thay";
+    }
+    else {
+        cout << "vi tri: " << x;
+    }
+
+    return 0;
+}
+
+int tim_kiem_nhi_phan(vi &arr, int x, int mode) {
+    int res = -1, l = 0, r = sz(arr) - 1;
+
+    while(l <= r) {
+        int m = l + (r - l) / 2;
+
+        if(arr[m] == x) {
+            res = m;
+
+            if(mode == 1) r = m - 1;
+            if(mode == 2) l = m + 1;
+            if(mode == 0) break;
+        }
+        else if(arr[m] < x) {
+            l = m + 1;
+        }
+        else {
+            r = m - 1;
+        }
+    }
+    return res;
+}
