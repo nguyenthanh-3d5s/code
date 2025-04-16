e4f0942c20693c49d31561b6b862970e5580770d
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-16 16:00:46 +0700

    Update chuong01_006.c

--- c/chuong01_006.c
+++ c/chuong01_006.c
@@ -7,22 +7,22 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    if(n%2 == 0) yes;
+    if(n % 2 == 0) yes;
     else no;
 
-    if(n%3 == 0 && n%5 == 0) yes;
+    if(n % 3 == 0 && n % 5 == 0) yes;
     else no;
 
-    if(n%3 == 0 && n%7 != 0) yes;
+    if(n % 3 == 0 && n % 7 != 0) yes;
     else no;
 
-    if(n%3 == 0 || n%7 == 0) yes;
+    if(n % 3 == 0 || n % 7 == 0) yes;
     else no;
 
     if(n > 30 && n < 50) yes;
     else no;
 
-    if(n >= 30 && (n%2 == 0 || n%3 == 0 || n%5 == 0)) yes;
+    if(n >= 30 && (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)) yes;
     else no;
 
     int r = n % 10;
@@ -30,13 +30,13 @@ int main() {
     if(n >= 10 && n <= 99 && (r == 2 || r == 3 || r == 5 || r == 7)) yes;
     else no;
 
-    if(n <= 100 && n%23 == 0) yes;
+    if(n <= 100 && n % 23 == 0) yes;
     else no;
 
     if(n < 10 || n > 20) yes;
     else no;
 
-    if(r%3 == 0) yes;
+    if(r % 3 == 0) yes;
     else no;
     
     return 0;
