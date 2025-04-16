b3ae7e835bbfc23a40b6a9ac5345d57b4012b4ce
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-16 23:20:57 +0700

    Update chuong01_012.c

--- c/chuong01_012.c
+++ c/chuong01_012.c
@@ -6,18 +6,24 @@ int main() {
     int t, n;
     scanf("%d %d", &t, &n);
 
-    if(t < 1 || t > 12 || n <= 0) printf("INVALID\n");
+    if(t < 1 || t > 12 || n <= 0) {
+        printf("INVALID\n");
+    }
     else {
         if(t == 2) {
             if(n % 400 == 0 || (n % 4 == 0 && n % 100 != 0)) {
                 printf("29\n");
             }
-            else printf("28\n");
+            else {
+                printf("28\n");
+            }
         }
         elif(t == 4 || t == 6 || t == 9 || t == 11) {
             printf("30\n");
         }
-        else printf("31\n");
+        else {
+            printf("31\n");
+        }
     }
 
     return 0;
