155ca11ec90a145a93fd29d5612cf5f093b68519
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-16 23:18:01 +0700

    Update chuong01_009.c

--- c/chuong01_009.c
+++ c/chuong01_009.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #define ll long long
-#define yes printf("YES\n")
-#define no printf("NO\n")
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    if(n % 400 == 0 || (n % 4 == 0 && n % 100 != 0)) yes;
-    else no;
+    if(n % 400 == 0 || (n % 4 == 0 && n % 100 != 0)) {
+        printf("YES\n")
+    }
+    else {
+        printf("NO\n")
+    }
 
     return 0;
 }
