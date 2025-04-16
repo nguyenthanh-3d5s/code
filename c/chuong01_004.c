13d7f7286fcc16a472a7216befb5881f2ad1137f
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-16 15:44:50 +0700

    Update chuong01_004.c

--- c/chuong01_004.c
+++ c/chuong01_004.c
@@ -4,8 +4,11 @@
 int main() {
     int r;
     scanf("%d", &r);
+    
     double cv = 2 * 3.14 * r;
     double dt = 3.14 * r * r;
+    
     printf("%.4lf %.4lf\n", cv, dt);
+    
     return 0;
 }
