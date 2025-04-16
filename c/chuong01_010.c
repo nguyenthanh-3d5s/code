4829a61186b117086bf3cf76767131506c976f3a
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-16 23:18:39 +0700

    Update chuong01_010.c

--- c/chuong01_010.c
+++ c/chuong01_010.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #define ll long long
-#define yes printf("YES\n")
-#define no printf("NO\n")
 
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    if(a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) yes;
-    else no;
+    if(a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
     
     return 0;
 }
