a20be5bc1d107b662e00863db0cdcdcbe8adbb4c
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-16 15:45:02 +0700

    Update chuong01_005.c

--- c/chuong01_005.c
+++ c/chuong01_005.c
@@ -5,7 +5,10 @@
 int main() {
     int x1, y1, x2, y2;
     scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    
     double kc = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    
     printf("%.2lf\n", kc);
+    
     return 0;
 }
