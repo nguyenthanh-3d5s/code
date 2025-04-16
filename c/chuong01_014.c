00b23f8ba6e53de5cbbfbf4a466329d0b7ba9ca2
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-16 22:41:03 +0700

    Create chuong01_014.c

---
+++ c/chuong01_014.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define ll long long
+#define elif else if
+
+int main() {
+    float a, b, c, d;
+    scanf("%f %f %f %f", &a, &b, &c, &d);
+
+    float tb = (a + b + c * 2 + d * 3) / 7;
+
+    if(tb >= 8) printf("GIOI\n");
+    elif(tb >= 6.5) printf("KHA\n");
+    elif(tb >= 5) printf("TRUNG BINH\n");
+    else printf("YEU\n");
+
+    return 0;
+}
