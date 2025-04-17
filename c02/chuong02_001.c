7427f8edcc7c36cd6c115db749531c445e058774
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-17 21:41:50 +0700

    Update chuong02_001.c

--- c02/chuong02_001.c
+++ c02/chuong02_001.c
@@ -6,13 +6,13 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int sum = 0;
+    ll sum = 0;
 
     for(int i = 1; i <= n; i++) {
         sum += i;
     }
 
-    printf("%d\n", sum);
+    printf("%lld\n", sum);
 
     return 0;
 }
