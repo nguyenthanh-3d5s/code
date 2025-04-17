8f374a9563a004abf162dc88b5dc6c739b88b2dd
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-17 20:52:55 +0700

    Create chuong01_027.c

---
+++ c/chuong01_027.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+#define elif else if
+
+int main() {
+    float a;
+    scanf("%f", &a);
+
+    int res = round(a);
+
+    printf("%d\n", res);
+
+    return 0;
+}
