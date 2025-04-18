74d6007a6657a0e0140c734a03f5e07dd1335b92
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-19 00:31:04 +0700

    Create chuong02_020.c

---
+++ c02/chuong02_020.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define ll long long
+#define elif else if
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n == 1) {
+        printf("-1\n");
+    }
+    else {
+        printf("%d\n", n / 2);
+
+        if(n % 2 == 0) {
+            for(int i = 1; i <= n / 2; i++) {
+                printf("2 ");
+            }
+        }
+        else {
+            for(int i = 1; i <= n / 2 - 1; i++) {
+                printf("2 ");
+            }
+            printf("3\n");
+        }
+    }
+
+    return 0;
+}
