65e9463b4e08fdf5e2857dc363cb2fc7794ecdcf
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-04-21 15:50:33 +0700

    Create chuong03_008.c

---
+++ c03/chuong03_008.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+#define elif else if
+#define FOR(i, a, b) for(int i = (a), _b = (b); i <= _b; i++)
+
+int main() {
+    ll a, b;
+    scanf("%lld %lld", &a, &b);
+
+    FOR(i, sqrt(a), sqrt(b)) {
+        printf("%lld ", 1ll * i * i);
+    }
+
+    return 0;
+}
