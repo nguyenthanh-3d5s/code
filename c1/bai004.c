d1a0434f0ffb0a727764c0623cf19f42d19928dc
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-15 16:39:17 +0700

    Create bai004.c

---
+++ c1/bai004.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define ll long long
+
+int main() {
+    int r;
+    scanf("%d", &r);
+    double cv = 2 * 3.14 * r;
+    double dt = 3.14 * r * r;
+    printf("%.4lf %.4lf\n", cv, dt);
+    return 0;
+}
