b484bd8e6c3c7bcba858d2247fc3b6d3d8085e53
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-15 16:43:02 +0700

    Create bai005.c

---
+++ c1/bai005.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+
+int main() {
+    int x1, y1, x2, y2;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    double kc = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    printf("%.2lf\n", kc);
+    return 0;
+}
