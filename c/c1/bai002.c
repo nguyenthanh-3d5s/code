c20bf1391e4516264225c8e689eb2941b5bc577c
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-24 15:54:11 +0700

    Create bai002.c

---
+++ c/c1/bai002.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    ll s = 1ll * a * (b + c) + 1ll * b * (a + c);
+    printf("%lld\n", s);
+    return 0;
+}
