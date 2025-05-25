93924f60c607adc7309a3905ace8edafb86979b1
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-25 22:36:06 +0700

    Create bai029.c

---
+++ c/c1/bai029.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int cb;
+    if(b % a == 0) {
+        cb = b / a;
+        if(b * cb == c && c * cb == d) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+    else {
+        printf("NO\n");
+    }
+    return 0;
+}
