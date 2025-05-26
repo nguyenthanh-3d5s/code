eaea1df931cf2c48c498a9e7f04f383b0cbd86d4
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-26 23:54:05 +0700

    Create bai014.c

---
+++ c/c2/bai014.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+#define FOR(i, a, b) for(int i = (a), b_ = (b); i <= b_; i++)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int tong = 0;
+    FOR(i, 1, n) {
+        tong += i * i * i;
+    }
+    printf("%d\n", tong);
+    return 0;
+}
