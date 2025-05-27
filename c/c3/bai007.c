acacf97f8d42b1977d5ebbdcc2bc22e4f7a20cd5
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-28 05:10:12 +0700

    Create bai007.c

---
+++ c/c3/bai007.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+#define FOR(i, a, b) for(int i = (a), b_ = (b); i <= b_; i++)
+#define REP(i, a, b) for(int i = (a), b_ = (b); i >= b_; i--)
+
+int cp(int n) {
+    int can = sqrt(n);
+    return (can * can == n) ? 1 : 0;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(cp(n)) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
+    return 0;
+}
