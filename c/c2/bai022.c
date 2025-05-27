89081ae73fd7b11d91b18677938cba919ee68a65
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-28 01:26:27 +0700

    Update bai022.c

--- c/c2/bai022.c
+++ c/c2/bai022.c
@@ -1,62 +1,61 @@
 #include <stdio.h>
 #include <math.h>
 #define ll long long
-#define endl printf("\n")
 #define FOR(i, a, b) for(int i = (a), b_ = (b); i <= b_; i++)
 #define REP(i, a, b) for(int i = (a), b_ = (b); i >= b_; i--)
 
 int main() {
     int n;
     scanf("%d", &n);
-    FOR(i, 1, n) FOR(j, 1, i) {
-        printf("*");
-        if(j == i) {
-            endl;
-        }
-    }
-    endl;
-    REP(i, n, 1) FOR(j, 1, i) {
-        printf("*");
-        if(j == i) {
-            endl;
-        }
-    }
-    endl;
-    FOR(i, 1, n) FOR(j, 1, n) {
-        if(j <= n - i) {
-            printf(" ");
-        }
-        else {
+    FOR(i, 1, n) {
+        FOR(j, 1, i) {
             printf("*");
         }
-        if(j == n) {
-            endl;
-        }
+        printf("\n");
     }
-    endl;
-    FOR(i, 1, n) FOR(j, 1, n) {
-        if(j >= i) {
+    printf("\n");
+    REP(i, n, 1) {
+        FOR(j, 1, i) {
             printf("*");
         }
-        else {
-            printf(" ");
-        }
-        if(j == n) {
-            endl;
-        }
+        printf("\n");
     }
-    endl;
-    FOR(i, 1, n) FOR(j, 1, i) {
-        if(i == 1 || i == n || j == 1 || j == i) {
-            printf("*");
+    printf("\n");
+    FOR(i, 1, n) {
+        FOR(j, 1, n) {
+            if(j <= n - i) {
+                printf(" ");
+            }
+            else {
+                printf("*");
+            }
         }
-        else {
-            printf(" ");
-        }
-        if(j == i) {
-            endl;
-        }
+        printf("\n");
     }
-    endl;
+    printf("\n");
+    FOR(i, 1, n) {
+        FOR(j, 1, n) {
+            if(j >= i) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    FOR(i, 1, n) {
+        FOR(j, 1, i) {
+            if(i == 1 || i == n || j == 1 || j == i) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
     return 0;
 }
