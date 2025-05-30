a43d77c3149763ee2e24f7d7675ee9cf129a055d
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-31 02:18:54 +0700

    Update bai012.c

--- c/c5/bai012.c
+++ c/c5/bai012.c
@@ -5,26 +5,7 @@
 #define FOR(i, a, b) for(int i = (a), b_ = (b); i < b_; i++)
 #define REP(i, a, b) for(int i = (a), b_ = (b); i > b_; i--)
 
-void kt(int f[]) {
-    f[0] = 0;
-    f[1] = 1;
-    FOR(i, 2, 21) {
-        f[i] = f[i - 1] + f[i - 2];
-    }
-}
-
-int check(int f[], int n) {
-    FOR(i, 0, 21) {
-        if(f[i] == n) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
 int main() {
-    int f[100];
-    kt(f);
     int n;
     scanf("%d", &n);
     int a[n];
