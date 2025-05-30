33ad5892fe4fbcbfd204c430a3b3196329fd1f5e
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-30 16:37:23 +0700

    Update bai009.c

--- c/c4/bai009.c
+++ c/c4/bai009.c
@@ -4,16 +4,18 @@
 #define FOR(i, a, b) for(int i = (a), b_ = (b); i <= b_; i++)
 #define REP(i, a, b) for(int i = (a), b_ = (b); i >= b_; i--)
 
+const int mod = 1e9 + 7;
+
 int binpow(int a, int b) {
     if(b == 0) {
         return 1;
     }
     int tmp = binpow(a, b / 2);
     if(b % 2 == 1) {
-        return tmp * tmp * a;
+        return (tmp % mod) * (tmp % mod) % mod * (a % mod) % mod;
     }
     else {
-        return tmp * tmp;
+        return (tmp % mod) * (tmp % mod) % mod;
     }
 }
 
