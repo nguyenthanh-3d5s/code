746d10d5bda58ac1d3abe9062eea35a5d81b9ba9
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-05-30 16:15:46 +0700

    Create bai004.c

---
+++ c/c4/bai004.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+#define FOR(i, a, b) for(int i = (a), b_ = (b); i <= b_; i++)
+#define REP(i, a, b) for(int i = (a), b_ = (b); i >= b_; i--)
+
+int s(int n) {
+    if(n == 0) {
+        return 0;
+    }
+    else {
+        if(n % 2 == 0) {
+            return n + s(n - 1);
+        }
+        else {
+            return -n + s(n - 1);
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", s(n));
+    return 0;
+}
