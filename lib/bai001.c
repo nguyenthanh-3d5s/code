476a0415b19b8f0d340bfc3ce3c527696ba4a284
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-06-08 13:16:02 +0700

    Create bai001.c

---
+++ lib/bai001.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define ll long long
+
+ll min(ll a, ll b) {
+    return a < b ? a : b;
+}
+
+ll max(ll a, ll b) {
+    return a > b ? a : b;
+}
+
+void swap(ll *a, ll *b) {
+    ll tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main() {
+
+    return 0;
+}
