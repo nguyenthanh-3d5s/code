ce5b62b84fabfa0e5e2f6cf499343599ef229b47
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-06-08 16:24:41 +0700

    Create bai003.c

---
+++ lib/bai003.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define ll long long
+
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int part(int arr[], int l, int r) {
+    int pivot = arr[r];
+    int i = l - 1;
+
+    for(int j = l; j < r; j++) {
+        if(arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    i++;
+    swap(&arr[i], &arr[r]);
+
+    return i;
+}
+
+void sort(int arr[], int l, int r) {
+    if(l < r) {
+        int m = part(arr, l, r);
+        sort(arr, l, m - 1);
+        sort(arr, m + 1, r);
+    }
+}
+
+int main() {
+
+    return 0;
+}
