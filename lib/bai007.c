1d65a1a1de11f94331c4ed771bb0a564bde8c00b
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-06-23 14:00:30 +0700

    Update bai007.c

--- lib/bai007.c
+++ lib/bai007.c
@@ -1,103 +1,59 @@
-#include <stdio.h>Add commentMore actions
-#include <stdlib.h>
-#include <stdbool.h>
+#include <stdio.h>
+#define ll long long
+#define n 10
+#define m 10
 
-#define M 317
+int gird[n][m];
 
-typedef struct {bool flag; int u;} node;
-typedef struct {int x, y, v;} queue;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
 
-static inline bool test(node arr[][M], int x, int y, int n, int m) {
-    return x < 0 || y < 0 || x >= n || y >= m || arr[x][y].flag == true || arr[x][y].u == 0;
-}
+int da[n * m];
+int db[n * m];
+int dc[n * m];
 
-static inline void push(node arr[][M], queue sub[], int x, int y, int l, int r) {
-    arr[x][y].flag = true;   sub[r].x = x;    sub[r].y = y;    sub[r].v = l;
-}
+int l = 0;
+int r = 1;
 
-void input(node [][M], int [], int, int);
-int bfs(node [][M], queue [], int [], int, int);
-void output(queue [], int);
-
-int main() {
-    int n, m;
-    scanf("%d %d ", &n, &m);
-
-    node arr[n][M];
-
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            scanf("%d ", &arr[i][j].u);
-            arr[i][j].flag = false;
-        }
-    }
-    
-    int a[2];
-    int b[2];
-    
-    input(arr, a, n, m);
-    input(arr, b, n, m);
-    
-    queue *sub = (queue*) malloc(n * m * sizeof(queue));
-    
-    push(arr, sub, b[0], b[1], -1, 0);
-    
-    int cnt = bfs(arr, sub, a, n, m);
-    
-    if(cnt == 0) {
-        printf("khong tim thay!");
-    }
-    else {
-        output(sub, cnt - 1);
-    }
-    
-    free(sub);
-    return 0;
-}
-
-void input(node arr[][M], int a[], int n, int m) {
-    do {
-        scanf("%d %d", &a[0], &a[1]);
-    } while(test(arr, a[0], a[1], n, m));
-}
-
-int bfs(node arr[][M], queue sub[], int a[], int n, int m) {
-    int l = 0;
-    int r = 1;
-    
-    int dx[] = {-1, 1, 0, 0};
-    int dy[] = {0, 0, -1, 1};
+void bfs(int x_, int y_, int a, int b) {
+    gird[x_][y_] = 0;
+    da[0] = x_;
+    db[0] = y_;
+    dc[0] = -1;
     
     while(l != r) {
-        int x = sub[l].x;
-        int y = sub[l].y;
+        int x = da[l];
+        int y = db[l];
 
-        for(int i = 0; i < 4; i++) {
-            int xx = x + dx[i];
-            int yy = y + dy[i];
-            
-            if(test(arr, xx, yy, n, m)) {
-                continue;
-            }
-            
-            push(arr, sub, xx, yy, l, r);
-            
-            r++;
-            
-            if(xx == a[0] && yy == a[1]) {
-                return r;
+        for(int k = 0; k < 4; k++) {
+            int i = x + dx[k];
+            int j = y + dy[k];
+
+            if(i >= 0 && i < n && j >= 0 && j < m && gird[i][j] == 1) {
+                gird[i][j] = 0;
+                da[r] = i;
+                db[r] = j;
+                dc[r++] = l;
+
+                if(i == a && j == b) {
+                    return;
+                }
             }
         }
-        
         l++;
     }
-    
-    return 0;
 }
 
-void output(queue sub[], int pos) {
+void trace() {
+    int pos = r - 1;
+
     while(pos != -1) {
-        printf("(%d, %d) ", sub[pos].x, sub[pos].y);
-        pos = sub[pos].v;
+        printf("(%d %d) ", da[pos], db[pos]);
+        pos = dc[pos];
     }
+}
+
+int main() {
+    
+    return 0;
 }
