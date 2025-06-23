59afad4f15efe740ae73ddb144aa8a5fb9df9b5f
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-06-23 09:40:15 +0700

    Create bai006.c

---
+++ lib/bai006.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define ll long long
+#define n 10
+#define m 10
+
+bool gird[n][m];
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+bool dfs(int x, int y, int a, int b) {
+    if(x == a && y == b) {
+        return 1;
+    }
+
+    gird[x][y] = 0;
+
+    for(int k = 0; k < 4; k++) {
+        int i = x + dx[k];
+        int j = y + dy[k];
+
+        if(i >= 0 && i < n && j >= 0 && j < m && gird[i][j] == 1) {
+            if(loang(i, j, a, b)) {
+                printf("(%d %d) ", i, j);
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+
+    return 0;
+}
