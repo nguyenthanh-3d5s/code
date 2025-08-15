dca099c20b23c0d926e99d8d0ce18302f9a352fa
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-08-15 19:02:38 +0700

    Update bai002.c

--- lib/bai002.c
+++ lib/bai002.c
@@ -4,17 +4,17 @@
 #define ll long long
 #define n_1 (int) 1e7
 
-bool arr_1[n_1];
+bool arr_1_[n_1];
 
 void sang() {
     for(int i = 2; i < n_1; i++) {
-        arr_1[i] = 1;
+        arr_1_[i] = 1;
     }
 
     for(int i = 2; i <= sqrt(n_1); i++) {
-        if(arr_1[i]) {
+        if(arr_1_[i]) {
             for(int j = i * i; j < n_1; j += i) {
-                arr_1[j] = 0;
+                arr_1_[j] = 0;
             }
         }
     }
