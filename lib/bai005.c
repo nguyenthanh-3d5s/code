cca99b6c67721aafc1118a6b1191cb55aae15592
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-08-16 00:50:11 +0700

    Update bai005.c

--- lib/bai005.c
+++ lib/bai005.c
@@ -8,7 +8,7 @@ int gird_1_[n_1][m_1];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-void flood(int x, int y) {
+void flo(int x, int y) {
     gird_1_[x][y] = 0;
 
     for(int k = 0; k < 4; k++) {
@@ -16,7 +16,7 @@ void flood(int x, int y) {
         int j = y + dy[k];
 
         if(i >= 0 && i < n_1 && j >= 0 && j < m_1 && gird_1_[i][j] == 1) {
-            flood(i, j);
+            flo(i, j);
         }
     }
 }
