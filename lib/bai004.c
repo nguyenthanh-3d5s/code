54e766344f34451afa01ecac42ba99899dc9d006
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-08-15 23:36:28 +0700

    Update bai004.c

--- lib/bai004.c
+++ lib/bai004.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #define ll long long
 
-int search(int arr[], int n, int x) {
-    int l = 0, r = n - 1;
-    int res = -1;
+int search(int arr_1_[], int n_1, int x) {
+    int l = 0, r = n_1 - 1;
+    int pos = -1;
 
     while(l <= r) {
         int m = l + (r - l) / 2;
 
-        if(arr[m] == x) {
-            res = m;
+        if(arr_1_[m] == x) {
+            pos = m;
             break;
         }
-        else if(arr[m] > x) {
+        else if(arr_1_[m] > x) {
             r = m - 1;
         }
         else {
@@ -20,7 +20,7 @@ int search(int arr[], int n, int x) {
         }
     }
 
-    return res;
+    return pos;
 }
 
 int main() {
