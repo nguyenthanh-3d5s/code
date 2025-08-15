ac947ae1f828557190ac8a370d207f10bb6e094d
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-08-15 23:51:42 +0700

    Update bai005.c

--- lib/bai005.c
+++ lib/bai005.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #define ll long long
-#define n 10
-#define m 10
+#define n_1 10
+#define m_1 10
 
-int gird[n][m];
+int gird_1_[n_1][m_1];
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
 void loang(int x, int y) {
-    gird[x][y] = 0;
+    gird_1_[x][y] = 0;
 
     for(int k = 0; k < 4; k++) {
         int i = x + dx[k];
         int j = y + dy[k];
 
-        if(i >= 0 && i < n && j >= 0 && j < m && gird[i][j] == 1) {
+        if(i >= 0 && i < n_1 && j >= 0 && j < m_1 && gird_1_[i][j] == 1) {
             loang(i, j);
         }
     }
