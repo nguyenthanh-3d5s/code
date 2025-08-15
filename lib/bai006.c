29a18288c58ddef8afb710a27f7244b80e7a8ca0
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-08-16 00:11:33 +0700

    Update bai006.c

--- lib/bai006.c
+++ lib/bai006.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #define ll long long
-#define n 10
-#define m 10
+#define n_1 10
+#define m_1 10
 
-int gird[n][m];
+int gird_1_[n_1][m_1];
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-int da[n * m];
-int db[n * m];
+int arr_1_[n_1 * m_1];
+int arr_2_[n_1 * m_1];
+
 int cnt = 0;
 
 void dfs(int x, int y, int a, int b) {
@@ -17,16 +18,16 @@ void dfs(int x, int y, int a, int b) {
         return;
     }
 
-    gird[x][y] = 0;
+    gird_1_[x][y] = 0;
 
     for(int k = 0; k < 4; k++) {
         int i = x + dx[k];
         int j = y + dy[k];
 
-        if(i >= 0 && i < n && j >= 0 && j < m && gird[i][j] == 1) {
+        if(i >= 0 && i < n_1 && j >= 0 && j < m_1 && gird_1_[i][j] == 1) {
             dfs(i, j, a, b);
-            da[cnt] = i;
-            db[cnt++] = j;
+            arr_1_[cnt] = i;
+            arr_2_[cnt++] = j;
             return;
         }
     }
