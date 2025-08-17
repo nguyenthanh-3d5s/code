5c6b4adc2d922f1a9e95d6906484d4b67eb7ba26
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-08-17 18:09:29 +0700

    Update bai001.c

--- lib/bai001.c
+++ lib/bai001.c
@@ -19,6 +19,19 @@ void swap(ll *a, ll *b) {
     *b = tmp;
 }
 
+void cache() {
+    while(getchar() != '\n') {}
+}
+
+void cache_fgets(char str[]) {
+    if(str[strlen(str) - 1] != '\n') {
+        cache();
+    }
+    else {
+        str[strlen(str) - 1] = '\0';
+    }
+}
+
 int main() {
     int x;
     while(scanf("%d", &x) != -1) {
