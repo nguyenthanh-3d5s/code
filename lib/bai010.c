dffdd668bc33f02cffa0b58fa29322cab5b1042d
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-08-20 00:47:23 +0700

    Update bai010.c

--- lib/bai010.c
+++ lib/bai010.c
@@ -23,9 +23,37 @@ void cache_fs(char str[]) {
 }
 
 int main() {
-    hs arr_1_[5];
-    for(int i = 0; i < 5; i++) {
-        
+    hs arr_1_[1000];
+    int n_1;
+
+    scanf("%d", &n_1);
+    cache_sf();
+
+    for(int i = 0; i < n_1; i++) {
+        fgets(arr_1_[i].ten, sizeof(arr_1_[i].ten), stdin);
+        cache_fs(arr_1_[i].ten);
+
+        scanf("%d", &arr_1_[i].ngay);
+        scanf("%d", &arr_1_[i].thang);
+        scanf("%d", &arr_1_[i].nam);
+        cache_sf();
+
+        fgets(arr_1_[i].lop, sizeof(arr_1_[i].lop), stdin);
+        cache_fs(arr_1_[i].lop);
+
+        fgets(arr_1_[i].ma_hs, sizeof(arr_1_[i].ma_hs), stdin);
+        cache_fs(arr_1_[i].ma_hs);
     }
+
+    for(int i = 0; i < n_1; i++) {
+        printf("%-30s", arr_1_[i].ten);
+
+        printf("%02d\\%02d\\%-15d", arr_1_[i].ngay, arr_1_[i].thang, arr_1_[i].nam);
+
+        printf("%-15s", arr_1_[i].lop);
+
+        printf("%s\n", arr_1_[i].ma_hs);
+    }
+
     return 0;
 }
