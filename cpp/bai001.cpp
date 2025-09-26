98b472292087eba0f9dc9541a0acfe689b1f47a9
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-26 20:30:49 +0700

    Update bai001.cpp

--- cpp/bai001.cpp
+++ cpp/bai001.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
