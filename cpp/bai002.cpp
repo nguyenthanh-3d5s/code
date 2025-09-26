e2cf3486170a3bce79cb7a0955629052af6c15ea
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-26 14:04:46 +0700

    Create bai002.cpp

---
+++ cpp/bai002.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a; cin >> a;
+    ll b = 1ll * a * a;
+
+    cout << b << "\n";
+
+    return 0;
+}
