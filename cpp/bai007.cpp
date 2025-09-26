42bf88ffdbe58df405c8295cbd4501a2ab472726
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-26 20:32:55 +0700

    Update bai007.cpp

--- cpp/bai007.cpp
+++ cpp/bai007.cpp
@@ -1,16 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-#define ll long long
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    float a = 0;
-    float b = 3;
-
-    printf("%.2f", -a/b ? -a/b : 0);
-
-    return 0;
-}
