edbc8709234c8ff2b144118b0b2dfedcf6d99696
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-26 23:20:38 +0700

    Update bai007.cpp

--- cpp/bai007.cpp
+++ cpp/bai007.cpp
@@ -11,9 +11,9 @@ int main() {
     int array[] = {2, 4, 1, 3, 5};
 
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n-1; j++) {
-            if(array[j] > array[j+1]) {
-                swap(array[j],array[j+1]);
+        for(int j = 0; j < n - 1; j++) {
+            if(array[j] > array[j + 1]) {
+                swap(array[j],array[j + 1]);
             }
         }
     }
