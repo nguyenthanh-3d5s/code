68c058f3d4312f7c595f24ee15f7d57f252e2dea
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-26 18:13:59 +0700

    Create bai003.cpp

---
+++ cpp/bai003.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //(a + b) mod m = (a mod m + b mod m) mod m
+    //(a − b) mod m = (a mod m − b mod m) mod m
+    //(a * b) mod m = (a mod m * b mod m) mod m
+
+    int a, b, c = 0;
+    cin >> a >> b;
+
+    while(b) {
+        if(b % 2 != 0) {
+            c += a;
+        }
+
+        b /= 2;
+        a *= 2;
+    }
+
+    cout << c << "\n";
+
+    return 0;
+}
