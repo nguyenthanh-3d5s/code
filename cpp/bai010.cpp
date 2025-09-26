9e6b27eb1d0878b09bf500041d1e883163c88cf7
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-26 23:50:09 +0700

    Create bai010.cpp

---
+++ cpp/bai010.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(a) (int) (a).size()
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s = "monkey";
+    
+    sort(s.begin(),s.end());
+
+    cout << s << "\n";
+
+    return 0;
+}
