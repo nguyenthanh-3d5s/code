a875d2ae5624fee9c59ce1eb1c8943fddfd96540
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-26 19:00:14 +0700

    Update bai005.cpp

--- cpp/bai005.cpp
+++ cpp/bai005.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    float a; cin >> a;
+
+    printf("%.2f \n", a);
+
+    return 0;
+}
