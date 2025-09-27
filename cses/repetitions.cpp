bc67cb320e6dad6e2f799bbe017636c29c6c0760
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-27 23:09:12 +0700

    Create repetitions.cpp

---
+++ cses/repetitions.cpp
@@ -0,0 +1,15 @@
+/*
+# đề bài:
+bạn được cho 1 chuỗi DNA bao gồm: A, C, G, T
+nhiệm vụ tìm chuỗi lặp lại dài nhất
+và in ra số lượng
+
+# biết:
+1 <= n <= 10^6
+
+# ví dụ:
+input:
+ATTCGGGA
+output:
+3
+*/
