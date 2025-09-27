2cd83fb7c0d0865cd37975176dda3ca775975112
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-27 23:17:47 +0700

    Create increasing_array.cpp

---
+++ cses/increasing_array.cpp
@@ -0,0 +1,18 @@
+/*
+# đề bài:
+cho 1 mảng gồm n số nguyên
+sửa mảng sao cho nó tăng dần
+mỗi lần di chuyển bạn có thể tăng bất kỳ phần tử nào +1
+hỏi số lần di chuyển tối thiểu là bao nhiêu
+
+# biết:
+1 <= n <= 10^5
+1 <= x <= 10^9
+
+# ví dụ:
+input:
+5
+3 2 5 1 7
+output:
+5
+*/
