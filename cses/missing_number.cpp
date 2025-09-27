60e4d095a996cc4879370ed01a06471fe61a789e
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-27 18:09:21 +0700

    Update missing_number.cpp

--- cses/missing_number.cpp
+++ cses/missing_number.cpp
@@ -1,1 +1,16 @@
+/*
+# đề bài:
+bạn được cung cấp tất cả các số từ 1 đến n
+ngoại trừ 1.
+nhiệm vụ của bạn là tìm số còn thiếu
 
+# biết:
+2 <= n <= 10^5
+
+# ví dụ:
+input:
+5
+2 3 1 5
+output:
+4
+*/
