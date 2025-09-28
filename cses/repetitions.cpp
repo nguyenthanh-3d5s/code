0e26eec31070abddf2e7d0145108208a39394f83
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-28 09:28:07 +0700

    Update repetitions.cpp

--- cses/repetitions.cpp
+++ cses/repetitions.cpp
@@ -13,3 +13,32 @@ ATTCGGGA
 output:
 3
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(a) (int) (a).size()
+
+typedef long long ll;
+
+int main() {
+    string chr; cin >> chr;
+    int max = 1;
+    int cnt = 1;
+
+    for(int i = 0; i < sz(chr) - 1; i++) {
+        if(chr[i] == chr[i + 1]) {
+            cnt++;
+            if(max < cnt) {
+                max = cnt;
+            }
+        }
+        else {
+            cnt = 1;
+        }
+    }
+
+    cout << max;
+
+    return 0;
+}
