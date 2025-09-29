79c314e8d0c77738037133e352c3cd3ecd2f2b12
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-29 20:48:11 +0700

    Update bai005_permutations.cpp

--- cses/bai005_permutations.cpp
+++ cses/bai005_permutations.cpp
@@ -21,3 +21,37 @@ input:
 output:
 NO SOLUTION
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n; cin >> n;
+
+    if(n < 5) {
+        if(n < 2) {
+            cout << 1;
+        }
+        else if(n < 4) {
+            cout << "NO SOLUTION";
+        }
+        else {
+            cout << "2 4 1 3";
+        }
+        return 0;
+    }
+
+    for(int i = n - n % 2; i > 1; i -= 2) {
+        cout << i << " ";
+    }
+
+    int tmp = (n % 2 == 0) ? n - 1 : n;
+
+    for(int i = tmp; i > 0; i -= 2) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
