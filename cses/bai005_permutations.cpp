a96878421fc4c5a669ddeb99845ad6fc064d33f6
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-09-29 15:46:10 +0700

    Update bai005_permutations.cpp

--- cses/bai005_permutations.cpp
+++ cses/bai005_permutations.cpp
@@ -1,1 +1,23 @@
+/*
+# đề bài:
+Cho số nguyên n
+n được gọi là đẹp
+nếu dãy số 1 đến n
+có thể sắp xếp thành 1 dãy
+mà các phần tử liền kề hơn kém nhau >1
+nếu không tồn tại in ra "NO SOLUTION"
 
+# biết:
+1 <= n <= 10^6
+
+# ví dụ:
+input:
+5
+output:
+4 2 5 3 1
+
+input:
+3
+output:
+NO SOLUTION
+*/
