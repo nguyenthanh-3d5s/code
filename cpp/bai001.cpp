7393444531e7c5fa2a7defcc7da40155ebeb3838
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-01 16:57:21 +0700

    Update bai001.cpp

--- cpp/bai001.cpp
+++ cpp/bai001.cpp
@@ -1,1 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int a; cin >> a;
+    int b; cin >> b;
+
+    cout << a / b * b << " ";
+    cout << (a + b - 1) / b * b << " ";
+
+    return 0;
+}
