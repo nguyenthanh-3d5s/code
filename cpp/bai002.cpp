746a7e946aff884b1f45ca6ce490e58daf4aefc2
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-02 17:16:17 +0700

    Create bai002.cpp

---
+++ cpp/bai002.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PI acos(-1)
+#define MOD (int) 1e9 + 7
+#define INF (int) 1e9 + 5
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int main() {
+    
+    return 0;
+}
