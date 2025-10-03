0792483964163b6a75f624056b868aa1a84238a1
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-03 09:50:42 +0700

    Create bai003.cpp

---
+++ cpp/bai003.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void tham_tri(int);
+void tham_chieu(int &);
+
+int main() {
+    int a; cin >> a;
+    int b; cin >> b;
+
+    tham_tri(a);
+    tham_chieu(b);
+
+    cout << a << " " << b;
+
+    return 0;
+}
+
+void tham_tri(int a) {
+    a = 0;
+}
+
+void tham_chieu(int &a) {
+    a = 0;
+}
