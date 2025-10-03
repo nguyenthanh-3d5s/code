c20e6150b9d06db907d85ada8208fc55b76904e2
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-03 10:43:51 +0700

    Create bai005.cpp

---
+++ cpp/bai005.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool chinh_phuong(int);
+
+int main() {
+    
+    return 0;
+}
+
+bool chinh_phuong(int a) {
+    int b = sqrt(a);
+    return b * b == a;
+}
