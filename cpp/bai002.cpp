773d529c407210d29352ab0e59b6039ceff3d075
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-03 09:34:04 +0700

    Update bai002.cpp

--- cpp/bai002.cpp
+++ cpp/bai002.cpp
@@ -11,6 +11,14 @@ typedef pair<int, int> ii;
 typedef vector<ii> vii;
 
 int main() {
-    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     return 0;
 }
