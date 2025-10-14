f3465f868768343686703d0470259744777430f6
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-14 13:32:39 +0700

    Update and rename ba015.cpp to bai015.cpp

---
+++ cpp/bai015.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void thua_so_nguyen_to(int);
+
+int main() {
+    thua_so_nguyen_to(120);
+    return 0;
+}
+
+void thua_so_nguyen_to(int n) {
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) {
+            cout << i;
+            int cnt = 0;
+
+            while(n % i == 0) {
+                n /= i;
+                cnt++;
+            }
+
+            cout << "^" << cnt;
+
+            if(n > 1) {
+                cout << " * ";
+            }
+        }
+    }
+
+    if(n > 1) {
+        cout << n << "^1";
+    }
+}
