a263274d21a4f26f520697e5f4baf91a7061fd24
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-14 14:07:55 +0700

    Update bai006.cpp

--- cpp/bai006.cpp
+++ cpp/bai006.cpp
@@ -8,14 +8,17 @@ int main() {
     return 0;
 }
 
-bool hoan_hao(int a) {
+bool hoan_hao(int n) {
     int sum = 0;
 
-    for(int i = 2; i <= sqrt(a); i++) {
-        if(a % i == 0) {
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) {
             sum += i;
-            sum += a / i;
+
+            if(i != n / i) {
+                sum += n / i;
+            }
         }
     }
-    return sum + 1 == a;
+    return sum == n;
 }
