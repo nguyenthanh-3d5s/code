dd504e8b44f777cb73398ba75fb6b1d1cb256ab3
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-18 01:09:49 +0700

    Update bai019.cpp

--- cpp/bai019.cpp
+++ cpp/bai019.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void sap_xep_chon(int[], int);
 bool cmp(int&, int&);
+void hoan_vi(int&, int&);
 
 int main() {
 
@@ -13,7 +14,7 @@ void sap_xep_chon(int arr[], int n) {
     for(int i = 0; i < n - 1; i++) {
         for(int j = i + 1; j < n; j++) {
             if(cmp(arr[i], arr[j])) {
-                swap(arr[i], arr[j]);
+                hoan_vi(arr[i], arr[j]);
             }
         }
     }
@@ -21,4 +22,10 @@ void sap_xep_chon(int arr[], int n) {
 
 bool cmp(int &a, int &b) {
     return a > b;
+}
+
+void hoan_vi(int &a, int &b) {
+    int r = a;
+    a = b;
+    b = r;
 }
