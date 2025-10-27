fd2cc0a592bd3c92b52e6177b0a7aac147893b0c
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-10-27 08:17:15 +0700

    Create bai022.cpp

---
+++ cpp/bai022.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(a) (int) (a).size()
+
+typedef vector<int> vi;
+
+void nhap(vi&);
+void xuat(vi&);
+
+int main() {
+    int n; cin >> n;
+    int m; cin >> m;
+    vi arr_01(n);
+    vi arr_02(m);
+    nhap(arr_01);
+    nhap(arr_02);
+    
+    int i = 0;
+    int j = 0;
+
+    while(i < n && j < m) {
+        if(arr_01[i] < arr_02[j]) {
+            cout << arr_01[i] << " ";
+            i++;
+        }
+        else {
+            cout << arr_02[j] << " ";
+            j++;
+        }
+    }
+
+    while(i < n) {
+        cout << arr_01[i] << " ";
+        i++;
+    }
+
+    while(j < n) {        
+        cout << arr_02[j] << " ";
+        j++;
+    }
+
+    return 0;
+}
+
+void nhap(vi &arr) {
+    for(int i = 0; i < sz(arr); i++) {
+        cin >> arr[i];
+    }
+}
+
+void xuat(vi &arr) {
+    for(int i = 0; i < sz(arr); i++) {
+        int j = *(arr.begin() + i);
+        cout << j << " ";
+    }
+}
