59674c21634bcb74a8db7b691fca3453e6df8f92
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2025-12-01 01:12:04 +0700

    Update bai001.c

--- linh_tinh/bai001.c
+++ linh_tinh/bai001.c
@@ -1,6 +1,30 @@
+/*
+Bài 1: Kiểm tra số nguyên tố
+input: 0 <= n <= 10^9
+output: "Yes" or "No"
+*/
+
 #include<stdio.h>
+#include<math.h>
+
+int check_ng_to(int);
 
 int main() {
-    printf("hello world \n");
+    int n;
+    scanf("%d", &n);
+    if(check_ng_to(n)) {
+        printf("Yes \n");
+    }
+    else printf("No \n");
     return 0;
+}
+
+int check_ng_to(int n) {
+    if(n < 2) return 0;
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
