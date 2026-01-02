e8218b4089f6d662236aefc3667cdd1b300db1d8
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-01-03 02:42:12 +0700

    Create bai007.c

---
+++ c/bai007.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool check_ng_to(int);
+void out_put(int);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    out_put(n);
+    return 0;
+}
+
+bool check_ng_to(int n) {
+    if(n < 2) {
+        return false;
+    }
+
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void out_put(int n) {
+    for(int i = 2; i <= n / 2; i++) {
+        if(check_ng_to(i) && check_ng_to(n - i)) {
+            printf("%d %d \n", i, n - i);
+        }
+    }
+}
