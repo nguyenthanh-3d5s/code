3c574bcc6d68f68f729a5b7886e473cfa068281e
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-01-14 16:58:02 +0700

    Create bai011.c

---
+++ c/bai011.c
@@ -0,0 +1,49 @@
+// Bài 11: Số T-prime nhiều test
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#define n (int) 1e6
+
+bool arr[n];
+
+void sang_ng_to();
+void out_put(int);
+
+int main() {
+    sang_ng_to();
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int m;
+        scanf("%d", &m);
+        out_put(m);
+    }
+    return 0;
+}
+
+void sang_ng_to() {
+    for(int i = 2; i < n; i++) {
+        arr[i] = true;
+    }
+
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(arr[i]) {
+            for(int j = i * i; j < n; j += i) {
+                arr[j] = false;
+            }
+        }
+    }
+}
+
+void out_put(int m) {
+    int c = 0;
+
+    for(int i = 2; i <= sqrt(m); i++) {
+        if(arr[i]) {
+            c++;
+        }
+    }
+
+    printf("%d \n", c);
+}
