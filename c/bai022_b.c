90c5e0540db8fcf72e5f5e4d7ce837274135a252
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-01-16 23:15:41 +0700

    Create bai022_b.c

---
+++ c/bai022_b.c
@@ -0,0 +1,33 @@
+// bài 22_b: số thuận nghịch
+
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+
+int tn(ll);
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        ll n;
+        scanf("%lld", &n);
+        if(tn(n)) {
+            printf("YES \n");
+        }
+        else printf("NO \n");
+    }
+    return 0;
+}
+
+int tn(ll n) {
+    ll rev = 0, m = n;
+    while(n != 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+    if(rev == m) {
+        return 1;
+    }
+    return 0;
+}
