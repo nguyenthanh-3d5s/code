240b78ba072aded391bfeabca72a7e0d54abbdd4
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-01-20 00:55:01 +0700

    Update bai005.c

--- c/bai005.c
+++ c/bai005.c
@@ -1,51 +1,15 @@
-// Bài 5: Kiểm tra số nguyên tố + chữ số nguyên tố
+// Bài 5: Kiểm tra số nguyên tố
+// kiểm tra chữ số nguyên tố
 
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
 
-bool check_ng_to(int);
-bool check_chu_so(int);
-void out_put(int, int);
+int check_ng_to(int);
+int check_chu_so(int);
 
 int main() {
-    int a, b;
+    int a, b, c = 0;
     scanf("%d %d", &a, &b);
-    out_put(a, b);
-    return 0;
-}
-
-bool check_ng_to(int n) {
-    if(n < 2) {
-        return false;
-    }
-
-    for(int i = 2; i <= sqrt(n); i++) {
-        if(n % i == 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-bool check_chu_so(int a) {
-    int b = a;
-
-    while(b) {
-        int r = b % 10;
-        if(r == 2 || r == 3 || r == 5 || r == 7) {
-            b /= 10;
-            continue;
-        }
-        return false;
-    }
-
-    return check_ng_to(a);
-}
-
-void out_put(int a, int b) {
-    int c = 0;
 
     for(int i = a; i <= b; i++) {
         if(check_chu_so(i)) {
@@ -54,4 +18,34 @@ void out_put(int a, int b) {
     }
 
     printf("%d \n", c);
+    return 0;
+}
+
+int check_ng_to(int n) {
+    if(n < 2) {
+        return 0;
+    }
+
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int check_chu_so(int a) {
+    int b = a;
+
+    while(b) {
+        int r = b % 10;
+        if(r == 2 || r == 3 || r == 5 || r == 7) {
+            b /= 10;
+            continue;
+        }
+        return 0;
+    }
+
+    return check_ng_to(a);
 }
