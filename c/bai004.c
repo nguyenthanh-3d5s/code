84c16ec21c0cbb6f174283c26fe28a7a45868770
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-01-19 23:51:44 +0700

    Update bai004.c

--- c/bai004.c
+++ c/bai004.c
@@ -1,43 +1,43 @@
-// Bài 4: Kiểm tra số nguyên tố với nhiều test
+// Bài 4: Kiểm tra số nguyên tố
+// sử dụng sàng nguyên tố
 
 #include <stdio.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdbool.h>
 #define n (int) 1e6
-#define yes printf("Yes\n")
-#define no printf("No\n")
 
 bool arr[n];
-
 void sang_ng_to();
-void out_put(int);
 
 int main() {
     sang_ng_to();
+
     int t;
     scanf("%d", &t);
+
     while(t--) {
-        int a;
-        scanf("%d", &a);
-        out_put(a);
+        int i;
+        scanf("%d", &i);
+        if(arr[i]) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
     }
     return 0;
 }
 
 void sang_ng_to() {
     for(int i = 2; i < n; i++) {
-        arr[i] = true;
+        arr[i] = 1;
     }
 
     for(int i = 2; i <= sqrt(n); i++) {
         if(arr[i]) {
             for(int j = i * i; j < n; j += i) {
-                arr[j] = false;
+                arr[j] = 0;
             }
         }
     }
-}
-
-void out_put(int i) {
-    arr[i] ? yes : no;
 }
