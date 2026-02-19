8255076e157ad647bc8ff0fb85c1f1033654c416
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-02-20 02:37:24 +0700

    Create bai002.c

---
+++ c/bai002.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE(a) (int) strlen(a)
+
+void CLEAN();
+void SAFE(char []);
+void GET(char [], int);
+
+int main() {
+    char note[20];
+    GET(note, sizeof(note));
+    printf("%s\n", note);
+    return 0;
+}
+
+void CLEAN() {
+    while(getchar() != '\n');
+}
+
+void SAFE(char note[]) {
+    if(note[SIZE(note) - 1] != '\n') {
+        CLEAN();
+    }
+    else {
+        note[SIZE(note) - 1] = '\0';
+    }
+}
+
+void GET(char note[], int a) {
+    fgets(note, a, stdin);
+    SAFE(note);
+}
