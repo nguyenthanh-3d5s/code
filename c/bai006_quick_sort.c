8ea29a9c50c4adeb859caed5c618102277e9376e
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-03-15 13:48:00 +0700

    Update bai006_quick_sort.c

--- c/bai006_quick_sort.c
+++ c/bai006_quick_sort.c
@@ -25,16 +25,37 @@ void SWAP(int *a, int *b) {
 }
 
 int PARTITION(int arr[], int l, int r) {
-    int val = arr[r];
+    int m = l + (r - l) / 2;
+    SWAP(&arr[m], &arr[r]);
     int i = l;
     int j = l;
 
     while(j < r) {
-        if(arr[j] < val) {
+        if(arr[j] < arr[r]) {
             SWAP(&arr[i], &arr[j]);
             i++;
         }
         j++;
+    }
+
+    SWAP(&arr[i], &arr[j]);
+    return i;
+}
+
+void QUICK_SORT(int arr[], int l, int r) {
+    while(l < r) {
+        int m = PARTITION(arr, l, r);
+
+        if(m - l < r - m) {
+            QUICK_SORT(arr, l, m - 1);
+            l = m + 1;
+        }
+        else {
+            QUICK_SORT(arr, m + 1, r);
+            r = m - 1;
+        }
+    }
+}        j++;
     }
 
     SWAP(&arr[i], &arr[j]);
