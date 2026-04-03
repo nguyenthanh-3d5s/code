2094ceeed0aba91865ff1ba08187911a24b707de
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-04-03 15:28:14 +0700

    Create bai002_sort.c

---
+++ search_sort/bai002_sort.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+void SWAP(int *, int *);
+void PART(int [],  int *, int *);
+void SORT(int [], int, int);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    SORT(arr, 0, n - 1);
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+void SWAP(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void PART(int arr[], int *low, int *high) {
+    int l = *low;
+    int r = *high;
+    int m = *low;
+
+    int mid = l + (r - l) / 2;
+    int val = arr[mid];
+
+    while(m < r) {
+        if(arr[m] == val) {
+            m++;
+        }
+        else if(arr[m] < val) {
+            SWAP(&arr[l], &arr[m]);
+            l++;
+            m++;
+        }
+        else {
+            SWAP(&arr[m], &arr[r]);
+            r--;
+        }
+    }
+
+    *low = l;
+    *high = r;
+}
+
+void SORT(int arr[], int l, int r) {
+    while(l < r) {
+        int low = l;
+        int high = r;
+        PART(arr, &low, &high);
+
+        if(low - l < r - high) {
+            SORT(arr, l, low - 1);
+            l = high + 1;
+        }
+        else {
+            SORT(arr, high + 1, r);
+            r = low - 1;
+        }
+    }
+}
