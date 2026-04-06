9e19dee4c5300601cda637fd1ed5267a5e877bda
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-04-06 21:26:44 +0700

    Create bai002_missing_number.c

---
+++ cses/introductory/bai002_missing_number.c
@@ -0,0 +1,26 @@
+/*
+Thời gian giới hạn: 1.00 giây
+Bộ nhớ giới hạn: 512 MB
+
+Bạn được cho tất cả các số từ 1, 2, ..., n ngoại trừ một số
+Nhiệm vụ của bạn là tìm số còn thiếu
+
+Đầu vào:
+Dòng đầu tiên chứa số nguyên n
+Dòng thứ hai chứa n - 1 số
+Mỗi số là khác nhau và nằm trong khoảng từ 1 đến n (bao gồm cả 1 và n)
+
+Đầu ra:
+In ra số còn thiếu
+
+Ràng buộc:
+2 <= n <= 
+
+# Ví dụ
+Đầu vào:
+5
+2 3 1 5
+
+Đầu ra:
+4
+*/
