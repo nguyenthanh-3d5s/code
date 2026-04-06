b4ac7b3ecfa5ec0dceda64b6241fd8ef926dcfcd
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-04-06 21:56:57 +0700

    Update bai002_sort.c

--- search_sort/bai002_sort.c
+++ search_sort/bai002_sort.c
@@ -7,11 +7,14 @@ void SORT(int [], int, int);
 int main() {
     int n;
     scanf("%d", &n);
+    
     int arr[n];
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    
     SORT(arr, 0, n - 1);
+    
     for(int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
