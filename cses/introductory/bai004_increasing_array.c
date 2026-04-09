727ba8b2b06239e6f1ef3314ea49d889ed8c67ee
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-04-09 22:37:16 +0700

    Update bai004_increasing_array.c

--- cses/introductory/bai004_increasing_array.c
+++ cses/introductory/bai004_increasing_array.c
@@ -2,29 +2,26 @@
 Thời gian: 1.00 giây
 Giới hạn bộ nhớ: 512 MB
 
-Bạn được cho một mảng gồm n số nguyên. Bạn muốn sửa mảng để nó trở thành mảng tăng dần, nghĩa là mỗi phần tử đều lớn hơn hoặc bằng phần tử đứng trước nó.
+Bạn được cho một mảng gồm n số nguyên
+Bạn muốn sửa mảng để nó trở thành mảng tăng dần
+nghĩa là mỗi phần tử đều lớn hơn hoặc bằng phần tử đứng trước nó
+Trong mỗi bước, bạn có thể tăng giá trị của bất kỳ phần tử nào lên một đơn vị
+Số bước ít nhất cần thực hiện là bao nhiêu?
 
-Trong mỗi bước, bạn có thể tăng giá trị của bất kỳ phần tử nào lên một đơn vị. Số bước ít nhất cần thực hiện là bao nhiêu?
+Đầu vào:
+Dòng đầu tiên chứa số nguyên n: kích thước của mảng
+Dòng thứ hai chứa n số nguyên x1, x2, …, xn: nội dung của mảng
 
-Đầu vào
+Đầu ra:
+In ra số bước ít nhất cần thực hiện
 
-Dòng đầu tiên chứa số nguyên n: kích thước của mảng.
-
-Dòng thứ hai chứa n số nguyên x1, x2, …, xn: nội dung của mảng.
-
-Đầu ra
-
-In ra số bước ít nhất cần thực hiện.
-
-Ràng buộc
-
+Ràng buộc:
 1 ≤ n ≤ 2 * 10^5
 1 ≤ xi ≤ 10^9
 
-Ví dụ
+# Ví dụ
 
 Đầu vào:
-
 5
 3 2 5 1 7
 
