2f44c217e5084c1c3e914861a327e7ed77c1d33c
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-04-09 19:44:25 +0700

    Create bai003_repetitions.c

---
+++ cses/introductory/bai003_repetitions.c
@@ -0,0 +1,25 @@
+/*
+Thời gian: 1.00 giây
+Giới hạn bộ nhớ: 512 MB
+
+Bạn được cho một chuỗi DNA: một xâu ký tự bao gồm các ký tự A, C, G và T
+Nhiệm vụ của bạn là tìm đoạn lặp dài nhất trong chuỗi
+Đó là một xâu con có độ dài lớn nhất chỉ chứa một loại ký tự
+
+Đầu vào:
+Dòng nhập duy nhất chứa một xâu có n ký tự.
+
+Đầu ra:
+In ra một số nguyên: độ dài của đoạn lặp dài nhất.
+
+Ràng buộc:
+1 ≤ n ≤ 10^6
+
+# Ví dụ
+
+Đầu vào:
+ATTCGGGA
+
+Đầu ra:
+3
+*/
