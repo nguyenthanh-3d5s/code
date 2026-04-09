ff262a4895358bd1d510d418dc9ebef51b9ab00a
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-04-09 23:11:15 +0700

    Update bai004_increasing_array.c

--- cses/introductory/bai004_increasing_array.c
+++ cses/introductory/bai004_increasing_array.c
@@ -28,3 +28,28 @@ Ràng buộc:
 Đầu ra:
 5
 */
+
+#include <stdio.h>
+#define ll long long
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    scanf("%d", &arr[0]);
+
+    ll cnt = 0;
+
+    for(int i = 1; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] < arr[i - 1]) {
+            cnt += (arr[i - 1] - arr[i]);
+            arr[i] = arr[i - 1];
+        }
+    }
+
+    printf("%lld", cnt);
+    return 0;
+}
