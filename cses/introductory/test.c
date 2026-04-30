1e7130058f0c9dd3b1be05f8dbd0aa4ac6e453ea
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-01 00:00:36 +0700

    Create test.c

---
+++ cses/introductory/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define n (int) 1e6
+
+int main() {
+    int m;
+    scanf("%d", &m);
+
+    if (m == 1) { printf("1\n"); return 0; }
+    if (m < 4) { printf("NO SOLUTION\n"); return 0; }
+
+    char note[n];
+    char *pos = note;
+
+    for (int i = 2; i <= m; i += 2) {
+        pos += sprintf(pos, "%d ", i);
+    }
+
+    for (int i = 1; i <= m; i += 2) {
+        pos += sprintf(pos, "%d ", i);
+    }
+
+    printf("%s", note);
+    return 0;
+}
