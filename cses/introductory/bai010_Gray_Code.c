5a6e8aa3115e7bbe9d68153b89a0b06575fbd73c
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-07 01:14:39 +0700

    Update bai010_Gray_Code.c

--- cses/introductory/bai010_Gray_Code.c
+++ cses/introductory/bai010_Gray_Code.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#define MASK(i) (1LL << (i))
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define GRAY(i) ((i) ^ ((i) >> 1))
 
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < MASK(n); i++) {
+        for(int j = n - 1; j >= 0; j--) {
+            printf("%d", BIT(GRAY(i), j));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
