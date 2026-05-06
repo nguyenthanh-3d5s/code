41f34d42bf9dd509eb24dd72c03e42b4fc8947ae
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-06 23:15:57 +0700

    Create bit_set.c

---
+++ bit/bit_set.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define MASK(i) (1LL << (i))
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define SET_ON(x, i) ((x) | MASK(i))
+#define SET_OFF(x, i) ((x) & ~MASK(i))
+
+int main() {
+    
+    return 0;
+}
