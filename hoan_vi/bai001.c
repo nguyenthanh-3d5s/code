c4942c49c7c3bd4ec0fcc00ca41a1d7494b638bb
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-20 15:25:23 +0700

    Update bai001.c

--- hoan_vi/bai001.c
+++ hoan_vi/bai001.c
@@ -32,33 +32,6 @@ void REVERSER(char note[], int l, int r) {
     }
 }
 
-void NEXT_PER(char note[], int len) {
-    int pos = -1;
-    for (int i = len - 2; i >= 0; i--) {
-        if (note[i] < note[i + 1]) {
-            pos = i;
-            break;
-        }
-    }
-
-    if (pos == -1) {
-        REVERSER(note, 0, len - 1);
-        return;
-    }
-
-    for (int i = len - 1; i > pos; i--) {
-        if (note[i] > note[pos]) {
-            SWAP(&note[i], &note[pos]);
-            break;
-        }
-    }
-
-    REVERSER(note, pos + 1, len - 1);
-}        l++;
-        r--;
-    }
-}
-
 void NEXT_PER(int arr[], int n) {
     int pos = -1;
     for (int i = n - 2; i >= 0; i--) {
