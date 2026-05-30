0eb051b08704c12eff2fafba8e60b6f522edecf3
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-31 00:00:56 +0700

    Update bai003_sort_string.c

--- search_sort/bai003_sort_string.c
+++ search_sort/bai003_sort_string.c
@@ -29,8 +29,7 @@ void PART(char note[], int *low, int *high) {
     int r = *high;
     int m = *low;
 
-    int mid = l + (r - l) / 2;
-    char val = note[mid];
+    char val = (note[l] + note[r]) / 2;
 
     while(m <= r) {
         if(note[m] == val) {
