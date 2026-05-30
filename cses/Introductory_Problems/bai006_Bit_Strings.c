d810afc9b2b6dcc16fa37044acf093ddeed43192
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-31 00:03:50 +0700

    Update bai006_Bit_Strings.c

--- cses/Introductory_Problems/bai006_Bit_Strings.c
+++ cses/Introductory_Problems/bai006_Bit_Strings.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#define MOD (int) (1e9+7)
 
+int BIN_NHAN(int, int);
+int BIN_LUY_THUA(int, int);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("%d", BIN_LUY_THUA(2, n));
+    return 0;
+}
+
+int BIN_NHAN(int a, int b) {
+    int res = 0;
+
+    while(b) {
+        if(b % 2 != 0) {
+            res = (res + a) % MOD;
+            b--;
+        }
+        a = (a * 2) % MOD;
+        b /= 2;
+    }
+
+    return res;
+}
+
+int BIN_LUY_THUA(int a, int b) {
+    int res = 1;
+
+    while(b) {
+        if(b % 2 != 0) {
+            res = BIN_NHAN(res, a);
+            b--;
+        }
+        a = BIN_NHAN(a, a);
+        b /= 2;
+    }
+
+    return res;
+}
