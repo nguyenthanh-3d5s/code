1fcd8622b11dc8143a047162c1c737ab12363ba0
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-31 00:04:53 +0700

    Update bai008_Coin_Piles.c

--- cses/Introductory_Problems/bai008_Coin_Piles.c
+++ cses/Introductory_Problems/bai008_Coin_Piles.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#define MIN(a, b) (a) < (b) ? (a) : (b)
+#define MAX(a, b) (a) > (b) ? (a) : (b)
 
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while(t--) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        int sum = a + b;
+        int min = MIN(a, b);
+        int max = MAX(a, b);
+
+        if(sum % 3 == 0 && min * 2 >= max) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
