c0f4aa92a6cca514902ab5bffaf1b1610d425a9e
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-31 00:02:27 +0700

    Update bai003_Repetitions.c

--- cses/Introductory_Problems/bai003_Repetitions.c
+++ cses/Introductory_Problems/bai003_Repetitions.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#define n (int) 1e6
 
+int main() {
+    char note[n];
+    scanf("%s", note);
+
+    int max = 1;
+    int cnt = 1;
+
+    for(int i = 1, len = strlen(note); i < len; i++) {
+        if(note[i - 1] != note[i]) {
+            if(max < cnt) {
+                max = cnt;
+            }
+            cnt = 1;
+        }
+        else {
+            cnt++;
+        }
+    }
+
+    printf("%d", (max < cnt) ? cnt : max);
+    return 0;
+}
