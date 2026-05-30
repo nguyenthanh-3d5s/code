51a60816036a2652f028bdf213f6e2eace2c8466
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-05-31 00:03:24 +0700

    Update bai005_Permutations.c

--- cses/Introductory_Problems/bai005_Permutations.c
+++ cses/Introductory_Problems/bai005_Permutations.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n == 1) {
+        printf("1");
+        return 0;
+    }
+
+    if(n < 4) {
+        printf("NO SOLUTION");
+        return 0;
+    }
+
+    for(int i = 2; i <= n; i += 2) {
+        printf("%d ", i);
+    }
+
+    for(int i = 1; i <= n; i += 2) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
