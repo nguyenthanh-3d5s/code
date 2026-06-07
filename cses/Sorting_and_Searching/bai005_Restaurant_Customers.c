1d7ccde0256a0c278bc9ef2274c146cf7cfc6898
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-06-07 20:19:45 +0700

    Update bai005_Restaurant_Customers.c

--- cses/Sorting_and_Searching/bai005_Restaurant_Customers.c
+++ cses/Sorting_and_Searching/bai005_Restaurant_Customers.c
@@ -11,26 +11,26 @@ int main() {
     n += n;
     
     int arr[n];
-    int sub[n];
+    int line[n];
     
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
 
         if(i % 2 == 0) {
-            sub[i] = 1;
+            line[i] = 1;
         }
         else {
-            sub[i] = -1;
+            line[i] = -1;
         }
     }
     
-    SORT(arr, sub, 0, n - 1);
+    SORT(arr, line, 0, n - 1);
 
     int cnt = 0;
     int max = 0;
     
     for(int i = 0; i < n; i++) {
-        cnt += sub[i];
+        cnt += line[i];
 
         if(max < cnt) {
             max = cnt;
@@ -48,7 +48,7 @@ void SWAP(int *a, int *b) {
     *b = temp;
 }
 
-void PART(int arr[], int sub[], int *low, int *high) {
+void PART(int arr[], int line[], int *low, int *high) {
     int l = *low;
     int r = *high;
     int m = *low;
@@ -61,13 +61,13 @@ void PART(int arr[], int sub[], int *low, int *high) {
         }
         else if(arr[m] < val) {
             SWAP(&arr[l], &arr[m]);
-            SWAP(&sub[l], &sub[m]);
+            SWAP(&line[l], &line[m]);
             l++;
             m++;
         }
         else {
             SWAP(&arr[m], &arr[r]);
-            SWAP(&sub[m], &sub[r]);
+            SWAP(&line[m], &line[r]);
             r--;
         }
     }
@@ -76,18 +76,18 @@ void PART(int arr[], int sub[], int *low, int *high) {
     *high = r;
 }
 
-void SORT(int arr[], int sub[], int l, int r) {
+void SORT(int arr[], int line[], int l, int r) {
     while(l < r) {
         int low = l;
         int high = r;
-        PART(arr, sub, &low, &high);
+        PART(arr, line, &low, &high);
 
         if(low - l < r - high) {
-            SORT(arr, sub, l, low - 1);
+            SORT(arr, line, l, low - 1);
             l = high + 1;
         }
         else {
-            SORT(arr, sub, high + 1, r);
+            SORT(arr, line, high + 1, r);
             r = low - 1;
         }
     }
