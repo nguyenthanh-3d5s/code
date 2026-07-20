782e2f4a5f50eb036224eed0835640de542a62bb
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-07-20 17:37:27 +0700

    Create bai003.c

---
+++ hello_algo/bai003.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *nestedLoop(int);
+
+int main() {
+    char *res = nestedLoop(2);
+    printf("%s", res);
+    free(res);
+    return 0;
+}
+
+char *nestedLoop(int n) {
+    int size = n * n * 26 + 1;
+    
+    char *res = malloc(size * sizeof(char));
+    res[0] = '\0';
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            char tmp[26];
+            snprintf(tmp, sizeof(tmp), "(%d, %d), ", i, j);
+            strncat(res, tmp, size - strlen(res) - 1);
+        }
+    }
+    
+    return res;
+}
