10075dcf8e68544fba6e14ca080ab0bcffa2e8a5
nguyenthanh-3d5s <nguyenthanh.3d5s@gmail.com>
2026-07-21 16:05:19 +0700

    Update bai009.c

--- hello_algo/bai009.c
+++ hello_algo/bai009.c
@@ -18,7 +18,7 @@ int main() {
 }
 
 int *randomNumbers(int n) {
-    int *nums = (int *)malloc(n * sizeof(int));
+    int *nums = malloc(n * sizeof(int));
     
     for(int i = 0; i < n; i++) {
         nums[i] = i + 1;
